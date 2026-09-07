df7f69e00779f5461968f8812aefda0fe5768445
hrvxf <166922826+hrvxf@users.noreply.github.com>
2026-09-07 18:23:50 +0100

    Add tap-to-reveal acronym prompt

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='actuary-recall-v4';
-const ASSETS=['./','./index.html','./styles.css?v=4','./app.js?v=4','./data.json?v=4','./manifest.webmanifest'];
+const CACHE='actuary-recall-v5';
+const ASSETS=['./','./index.html','./styles.css?v=5','./app.js?v=5','./data.json?v=5','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{if(event.request.mode==='navigate'||['script','style'].includes(event.request.destination)){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request)));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))});
